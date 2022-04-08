3c5512a26c1a28f42ede49a8181433e3122e86d9
idoodi <idoodi@ya.ru>
2022-04-08 05:39:48 +0300

    1

--- src/components/cash(hooks).jsx
+++ src/components/cash(hooks).jsx
@@ -1,9 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { addCashAction, getCashAction } from "../redux/sync/cash/cashActions";
 
-
 const App = () => {
+    //useSelector просто получает стейт
     const cash = useSelector(state => state.cashState.cash)
+    //useDispatch диспатчит(отправляет экшны в стейт)
     const dispatch = useDispatch()
     return (
         <div>
